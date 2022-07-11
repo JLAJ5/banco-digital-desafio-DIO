a77879d919d8746fa1ee3a371ef7ce834e94867d
daniel-baquini <danielbaquini@protonmail.com>
2022-07-11 09:17:19 -0300

    Adicionada uma factory de contas, com isso, conseguimos delegar a responsabilidade de instanciacao para uma classe em especifico, tirando essa acao da classe main, que ficara responsavel apenas por obter dados do usuario

---
+++ src/entities/ContaFactory.java
@@ -0,0 +1,18 @@
+package entities;
+
+import entities.Exceptions.DomainException;
+
+public class ContaFactory {
+    
+    public Conta getInstance(String tipoDeConta, Cliente cliente) throws DomainException {
+        if(tipoDeConta == "Corrente") {
+            return new ContaCorrente(cliente);
+        }
+        else if(tipoDeConta == "Poupança") {
+            return new ContaPoupanca(cliente);
+        }
+
+        throw new DomainException("O tipo de conta fornecido foi inválido.");
+    }
+
+}
