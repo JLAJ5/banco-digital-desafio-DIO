0cacc44045a2d7dde79766fde4a726c7b5cdcca3
daniel-baquini <danielbaquini@protonmail.com>
2022-07-11 09:25:14 -0300

    Adicionado um campo de id e um contator interno para este id (que funciona como uma chave primaria em um banco de dados sql). O objetivo do id e permitir que o usuario possa referenciar um cliente em especifico

--- src/entities/Cliente.java
+++ src/entities/Cliente.java
@@ -2,7 +2,28 @@ package entities;
 
 public class Cliente {
 
+	private static int contatorDeID = 1;
+
+	private Number id;
 	private String nome;
+
+	public Cliente() {
+		this.id = Cliente.contatorDeID;
+		Cliente.contatorDeID++;
+	}
+
+	public Cliente(String nome) {
+		this();
+		this.nome = nome;
+	}
+
+	public Number getId() {
+		return id;
+	}
+
+	public void setId(Number id) {
+		this.id = id;
+	}	
 
 	public String getNome() {
 		return nome;
@@ -11,5 +32,10 @@ public class Cliente {
 	public void setNome(String nome) {
 		this.nome = nome;
 	}
+
+	@Override
+	public String toString() {
+		return "Cliente [id=" + id + ", nome=" + nome + "]";
+	}	
 
 }
