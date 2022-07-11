4ab39a2364313d3edc16c075c52a6b67f4f59167
daniel-baquini <danielbaquini@protonmail.com>
2022-07-11 09:00:15 -0300

    Adicionado um novo package para conter as excecoes personalizadas. A DomainException é uma excecao que devera ser lancada quando alguma regra de negocio nao for respeitada

--- src/entities/Conta.java
+++ src/entities/Conta.java
@@ -1,5 +1,7 @@
 package entities;
 
+import entities.Exceptions.DomainException;
+
 public abstract class Conta implements IConta {
 	
 	private static final int AGENCIA_PADRAO = 1;
@@ -18,6 +20,9 @@ public abstract class Conta implements IConta {
 
 	@Override
 	public void sacar(double valor) {
+		if(valor > this.saldo) {
+			throw new DomainException("O valor a ser sacado é maior que o saldo disponível.");
+		}
 		saldo -= valor;
 	}
 
---
+++ src/entities/Exceptions/DomainException.java
@@ -0,0 +1,9 @@
+package entities.Exceptions;
+
+public class DomainException extends RuntimeException {
+    
+    public DomainException(String msg) {
+        super(msg);
+    }
+
+}
