5e5a60946e00f688535d790fbd7b71b356792384
daniel-baquini <danielbaquini@protonmail.com>
2022-07-11 09:41:56 -0300

    Adicionada funcionalidade na classe 'Main' que o usuario possa cadastrar clientes

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,8 @@
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Scanner;
+
 import entities.Cliente;
 import entities.Conta;
 import entities.ContaCorrente;
@@ -24,6 +29,25 @@ public class Main {
 			System.out.println("Ocorreu um erro na execução do programa: " + e.getMessage());
 		}
 
+	}
+
+	public static List<Cliente> obterClientes(Scanner sc) {
+		List<Cliente> clientes = new ArrayList<>();
+
+		System.out.println("\n\nVocê está na etapa de cadastro de clientes.\n\n");
+		char cadastrarOutroCliente = 'S';
+		while(cadastrarOutroCliente == 'S') {
+			System.out.print("Qual o nome do cliente? ");
+			Cliente novoCliente = new Cliente(sc.nextLine());
+			System.out.println("Dados do novo cliente: " + novoCliente);
+			clientes.add(novoCliente);
+
+			System.out.print("Deseja cadastrar outro cliente (S/N)? ");
+			cadastrarOutroCliente = sc.nextLine().charAt(0);
+			System.out.println();
+		}
+
+		return clientes;
 	}
 
 }
